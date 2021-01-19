39d10efbe565ada168c26b47a6fa98bfcc2002d9
myusername <tzirincarlos@outlook.com>
2021-01-19 14:14:30 -0600

    Impresión en recepción

--- app/src/main/java/com/dts/tom/Transacciones/Recepcion/frm_recepcion_datos.java
+++ app/src/main/java/com/dts/tom/Transacciones/Recepcion/frm_recepcion_datos.java
@@ -3216,14 +3216,14 @@ public class frm_recepcion_datos extends PBase {
 
                 if (BeTransReDet.Presentacion.EsPallet){
 
-                    //ImprimirBarra
+                    Imprimir_Licencia();
 
                     progress.cancel();
 
                 }else{
                     if (BeTransReDet.Presentacion.Imprime_barra){
                         progress.cancel();
-                        //ImprimirBarra
+                        Imprimir_Barra();
 
                     }
                 }
@@ -3252,20 +3252,96 @@ public class frm_recepcion_datos extends PBase {
 
     }
 
-    private void Imprimir_Barra(){
+    private void Imprimir_Licencia(){
         try{
 
             //CM_20210112: Impresión de barras.
             BluetoothConnection printerIns= new BluetoothConnection(gl.MacPrinter);
-            ZebraPrinter zPrinterIns = ZebraPrinterFactory.getInstance(printerIns);
-            zPrinterIns.sendCommand("! U1 setvar \"device.languages\" \"zpl\"\r\n");
-            zPrinterIns.sendCommand("^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ");
-            Thread.sleep(500);
+            printerIns.open();
 
-            // Close the connection to release resources.
-            printerIns.close();
+            if (printerIns.isConnected()){
+                ZebraPrinter zPrinterIns = ZebraPrinterFactory.getInstance(printerIns);
+                //zPrinterIns.sendCommand("! U1 setvar \"device.languages\" \"zpl\"\r\n");
 
-            Looper.myLooper().quit();
+
+                String zpl = String.format("^XA \n" +
+                                "^MMT \n" +
+                                "^PW700 \n" +
+                                "^LL0406 \n" +
+                                "^LS0 \n" +
+                                "^FT171,61^A0I,25,14^FH^FD%1$s^FS \n" +
+                                "^FT550,61^A0I,25,14^FH^FD%2$s^FS \n" +
+                                "^FT670,306^A0I,25,14^FH^FD%3$s^FS \n" +
+                                "^FT292,61^A0I,25,24^FH^FDBodega:^FS \n" +
+                                "^FT670,61^A0I,25,24^FH^FDEmpresa:^FS \n" +
+                                "^FT670,367^A0I,25,24^FH^FDTOMIMS, WMS.  Product Barcode^FS \n" +
+                                "^FO2,340^GB670,0,14^FS \n" +
+                                "^BY3,3,160^FT670,131^BCI,,Y,N \n" +
+                                "^FD%4$s^FS \n" +
+                                "^PQ1,0,1,Y " +
+                                "^XZ",gl.CodigoBodega, gl.gNomEmpresa,
+                        BeProducto.Codigo+" - "+BeProducto.Nombre,
+                        "$"+pNumeroLP);
+
+                zPrinterIns.sendCommand(zpl);
+
+
+                Thread.sleep(500);
+
+                // Close the connection to release resources.
+                printerIns.close();
+
+            }else{
+                mu.msgbox("No se pudo obtener conexión con la impresora");
+            }
+
+        }catch (Exception e){
+            mu.msgbox("Imprimir_barra: "+e.getMessage());
+        }
+    }
+
+    private void Imprimir_Barra(){
+        try{
+
+                            //CM_20210112: Impresión de barras.
+                BluetoothConnection printerIns= new BluetoothConnection(gl.MacPrinter);
+                printerIns.open();
+
+                if (printerIns.isConnected()){
+                    ZebraPrinter zPrinterIns = ZebraPrinterFactory.getInstance(printerIns);
+                    //zPrinterIns.sendCommand("! U1 setvar \"device.languages\" \"zpl\"\r\n");
+
+
+                    String zpl = String.format("^XA \n" +
+                                    "^MMT \n" +
+                                    "^PW700 \n" +
+                                    "^LL0406 \n" +
+                                    "^LS0 \n" +
+                                    "^FT171,61^A0I,25,14^FH^FD%1$s^FS \n" +
+                                    "^FT550,61^A0I,25,14^FH^FD%2$s^FS \n" +
+                                    "^FT670,306^A0I,25,14^FH^FD%3$s^FS \n" +
+                                    "^FT292,61^A0I,25,24^FH^FDBodega:^FS \n" +
+                                    "^FT670,61^A0I,25,24^FH^FDEmpresa:^FS \n" +
+                                    "^FT670,367^A0I,25,24^FH^FDTOMIMS, WMS.  Product Barcode^FS \n" +
+                                    "^FO2,340^GB670,0,14^FS \n" +
+                                    "^BY3,3,160^FT670,131^BCI,,Y,N \n" +
+                                    "^FD%4$s^FS \n" +
+                                    "^PQ1,0,1,Y " +
+                                    "^XZ",gl.CodigoBodega, gl.gNomEmpresa,
+                            BeProducto.Codigo+" - "+BeProducto.Nombre,
+                            (pNumeroLP!="")?"$"+pNumeroLP:BeProducto.Codigo);
+
+                    zPrinterIns.sendCommand(zpl);
+
+
+                    Thread.sleep(500);
+
+                    // Close the connection to release resources.
+                    printerIns.close();
+
+                }else{
+                    mu.msgbox("No se pudo obtener conexión con la impresora");
+                }
 
         }catch (Exception e){
             mu.msgbox("Imprimir_barra: "+e.getMessage());
@@ -3307,6 +3383,9 @@ public class frm_recepcion_datos extends PBase {
                     clsBeProducto_Presentacion bePresentacion = new clsBeProducto_Presentacion();
 
                     bePresentacion = stream(BeProducto.Presentaciones.items).where(c->c.IdPresentacion==IdPreseSelect).first();
+
+
+                    BeTransReDet.Presentacion = bePresentacion;
 
                     if(!bePresentacion.EsPallet){
                         Factor = bePresentacion.Factor;
