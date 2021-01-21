5d281c5404c38cd6f475d013916526c04c6fdf3e
Erik José Calderón <yejc27@hotmail.com>
2021-01-21 07:08:53 -0600

    No validar tamaño 16 en barra de pallet.

--- app/src/main/java/com/dts/tom/Transacciones/Recepcion/frm_recepcion_datos.java
+++ app/src/main/java/com/dts/tom/Transacciones/Recepcion/frm_recepcion_datos.java
@@ -5045,33 +5045,33 @@ public class frm_recepcion_datos extends PBase {
 
     }
 
-    private void msgAskImprimir(String msg) {
-        try{
-            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-
-            dialog.setTitle(R.string.app_name);
-            dialog.setMessage("¿" + msg + "?");
-
-            dialog.setCancelable(false);
-
-            dialog.setIcon(R.drawable.ic_quest);
-
-            dialog.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                }
-            });
-
-            dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {                }
-            });
-
-            dialog.show();
-
-        }catch (Exception e){
-            addlog(new Object(){}.getClass().getEnclosingMethod().getName(),e.getMessage(),"");
-        }
-
-    }
+//    private void msgAskImprimir(String msg) {
+//        try{
+//            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+//
+//            dialog.setTitle(R.string.app_name);
+//            dialog.setMessage("¿" + msg + "?");
+//
+//            dialog.setCancelable(false);
+//
+//            dialog.setIcon(R.drawable.ic_quest);
+//
+//            dialog.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int which) {
+//                }
+//            });
+//
+//            dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int which) {                }
+//            });
+//
+//            dialog.show();
+//
+//        }catch (Exception e){
+//            addlog(new Object(){}.getClass().getEnclosingMethod().getName(),e.getMessage(),"");
+//        }
+//
+//    }
 
     private void doExit(){
         try{
