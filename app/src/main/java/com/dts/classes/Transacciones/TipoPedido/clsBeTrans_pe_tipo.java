648be142ca24299601aedd52d930a8034b16f949
Efren Buch <48444458+tzirincarlos@users.noreply.github.com>
2021-07-02 09:41:51 -0600

    GT 02072021 se agrega generar_pedido y tipoIngresoOC a clsBeTrans_pe_tipo

--- app/src/main/java/com/dts/classes/Transacciones/TipoPedido/clsBeTrans_pe_tipo.java
+++ app/src/main/java/com/dts/classes/Transacciones/TipoPedido/clsBeTrans_pe_tipo.java
@@ -13,13 +13,15 @@ public class clsBeTrans_pe_tipo {
     @Element(required=false) public boolean ImprimeBarrasPicking=false;
     @Element(required=false) public boolean ImprimeBarrasPacking=false;
     @Element(required=false) public boolean requerir_documento_ref =false;
+    @Element(required=false) public boolean Generar_pedido_ingreso_bodega_destino =false;
+    @Element(required=false) public int IdTipoIngresoOC=1;
 
     public clsBeTrans_pe_tipo() {
     }
 
     public clsBeTrans_pe_tipo(int IdTipoPedido,String Nombre,String Descripcion,boolean Preparar,
                               boolean Verificar,boolean ReservaStock,boolean ImprimeBarrasPicking,boolean ImprimeBarrasPacking,
-                              boolean requerir_documento_ref
+                              boolean requerir_documento_ref,boolean Generar_pedido_ingreso_bodega_destino,int IdTipoIngresoOC
     ) {
 
         this.IdTipoPedido=IdTipoPedido;
@@ -31,6 +33,8 @@ public class clsBeTrans_pe_tipo {
         this.ImprimeBarrasPicking=ImprimeBarrasPicking;
         this.ImprimeBarrasPacking=ImprimeBarrasPacking;
         this.requerir_documento_ref = requerir_documento_ref;
+        this.Generar_pedido_ingreso_bodega_destino= Generar_pedido_ingreso_bodega_destino;
+        this.IdTipoIngresoOC = IdTipoIngresoOC;
 
     }
 
@@ -88,6 +92,18 @@ public class clsBeTrans_pe_tipo {
     }
     public void setrequerir_documento_ref(boolean value) {
         requerir_documento_ref=value;
+    }
+    public boolean getGenerar_pedido_ingreso_bodega_destino() {
+        return Generar_pedido_ingreso_bodega_destino;
+    }
+    public void setGenerar_pedido_ingreso_bodega_destino(boolean value) {
+        Generar_pedido_ingreso_bodega_destino=value;
+    }
+    public int getIdTipoIngresoOC() {
+        return IdTipoPedido;
+    }
+    public void setIdTipoIngresoOC(int value) {
+        IdTipoPedido=value;
     }
 
 }
