dcdddab61eef6dfde6b8efc2f3317d291d3bd360
ejcalderongt <ejcalderon02@yahoo.com>
2021-12-22 12:22:10 -0600

    version 4.5.36

--- app/src/main/java/com/dts/tom/MainActivity.java
+++ app/src/main/java/com/dts/tom/MainActivity.java
@@ -91,7 +91,7 @@ public class MainActivity extends PBase {
     private boolean idle=false;
 
     private String rootdir = Environment.getExternalStorageDirectory() + "/WMSFotos/";
-    private String version="4.5.35";
+    private String version="4.5.36";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
