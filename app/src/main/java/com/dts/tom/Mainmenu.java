4804d9e6285a76ce37a5e2c29627fc2abade7c78
Efren Gustavo tuyuc <lemonde066@hotmail.com>
2022-01-12 15:32:44 -0600

    GT12012021: opcion implosion equivale a packing del bof

--- app/src/main/java/com/dts/tom/Mainmenu.java
+++ app/src/main/java/com/dts/tom/Mainmenu.java
@@ -184,7 +184,7 @@ public class Mainmenu extends PBase {
                         items.add(item);
                     }
 
-                    if (gl.beOperador.RolOperador.ListMenuRolOp.items.get(i).MenuSistemaOp.Nombre.equals("Implosión")) {
+                    if (gl.beOperador.RolOperador.ListMenuRolOp.items.get(i).MenuSistemaOp.Nombre.equals("Packing")) {
                         item = clsCls.new clsMenu();
                         item.ID=4;item.Icon=4;item.Name="Implosión";item.cant=-1;
                         items.add(item);
