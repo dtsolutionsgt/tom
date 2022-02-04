a701323a8204fda1711023234eb1f08195e0919d
Carolina Fuentes <carolinakfk@hotmail.com>
2022-02-04 17:57:16 -0600

    //#CKFK 20220125 Cambios en la clase trans_oc_det_lote

--- app/src/main/java/com/dts/classes/Transacciones/OrdenCompra/Trans_oc_det_lote/clsBeTrans_oc_det_lote.java
+++ app/src/main/java/com/dts/classes/Transacciones/OrdenCompra/Trans_oc_det_lote/clsBeTrans_oc_det_lote.java
@@ -1,6 +1,10 @@
 package com.dts.classes.Transacciones.OrdenCompra.Trans_oc_det_lote;
 
 
+import com.dts.classes.Mantenimientos.Producto.Producto_Presentacion.clsBeProducto_Presentacion;
+import com.dts.classes.Mantenimientos.Producto.Producto_Presentacion.clsBeProducto_PresentacionList;
+import com.dts.classes.Mantenimientos.Unidad_medida.clsBeUnidad_medida;
+
 import org.simpleframework.xml.Element;
 
 public class clsBeTrans_oc_det_lote {
@@ -18,13 +22,26 @@ public class clsBeTrans_oc_det_lote {
     @Element(required=false) public String Lic_Plate;
     @Element(required=false) public String Ubicacion;
     @Element(required=false) public Boolean IsNew=false;
+    //#CKFK20220131 Campos agregados para poder saber en que unidad de medida se hizo la OC
+    @Element(required=false) public int IdPresentacion = 0;
+    @Element(required=false) public int IdUnidadMedidaBasica = 0;
+    //#CKFK20220131 Campos de bitácora agregados
+    @Element(required=false) public String User_agr = "";
+    @Element(required=false) public String Fec_agr ="1900-01-01T00:00:01";
+    @Element(required=false) public String User_mod = "";
+    @Element(required=false) public String Fec_mod ="1900-01-01T00:00:01";
+    @Element(required=false) public clsBeProducto_Presentacion Presentacion = new clsBeProducto_Presentacion();
+    @Element(required=false) public clsBeUnidad_medida UnidadMedida = new clsBeUnidad_medida();
 
     public clsBeTrans_oc_det_lote() {
     }
 
     public clsBeTrans_oc_det_lote(int IdOrdenCompraEnc,int IdOrdenCompraDet,int IdOrdenCompraDetLote,int IdProductoBodega,
                                   int No_linea,String Codigo_producto,double Cantidad,double Cantidad_recibida,
-                                  String Lote,String Fecha_vence, String Lic_Plate, String Ubicacion) {
+                                  String Lote,String Fecha_vence, String Lic_Plate, String Ubicacion,
+                                  int IdPresentacion,int IdUnidadMedidaBasica,String User_agr,String Fec_agr,
+                                  String User_mod, String Fec_mod, clsBeProducto_Presentacion Presentacion,
+                                  clsBeUnidad_medida UnidadMedida) {
 
         this.IdOrdenCompraEnc=IdOrdenCompraEnc;
         this.IdOrdenCompraDet=IdOrdenCompraDet;
@@ -38,8 +55,15 @@ public class clsBeTrans_oc_det_lote {
         this.Fecha_vence =Fecha_vence;
         this.Lic_Plate = Lic_Plate;
         this.Ubicacion = Ubicacion;
+        this.IdPresentacion = IdPresentacion;
+        this.IdUnidadMedidaBasica = IdUnidadMedidaBasica;
+        this.User_agr = User_agr;
+        this.Fec_agr = Fec_agr;
+        this.User_mod = User_mod;
+        this.Fec_mod = Fec_mod;
+        this.Presentacion = Presentacion;
+        this.UnidadMedida = UnidadMedida;
     }
-
 
     public int getIdOrdenCompraEnc() {
         return IdOrdenCompraEnc;
@@ -114,6 +138,61 @@ public class clsBeTrans_oc_det_lote {
     }
     public void setUbicacion(String value) {
         Ubicacion=value;
+    }
+
+    public int getIdPresentacion() {
+        return IdPresentacion;
+    }
+    public void setIdPresentacion(int value) {
+        IdPresentacion=value;
+    }
+
+    public int getIdUnidadMedidaBasica() {
+        return IdUnidadMedidaBasica;
+    }
+    public void setIdUnidadMedidaBasica(int value) {
+        IdUnidadMedidaBasica=value;
+    }
+    public String getUser_agr() {
+        return User_agr;
+    }
+    public void setUser_agr(String value) {
+        User_agr=value;
+    }
+
+    public String getFec_agr() {
+        return Fec_agr;
+    }
+    public void setFec_agr(String value) {
+        Fec_agr=value;
+    }
+
+    public String getUser_mod() {
+        return User_mod;
+    }
+    public void setUser_mod(String value) {
+        User_mod=value;
+    }
+
+    public String getFec_mod() {
+        return Fec_mod;
+    }
+    public void setFec_mod(String value) {
+        Fec_mod=value;
+    }
+
+    public clsBeUnidad_medida getUnidadMedida() {
+        return UnidadMedida;
+    }
+    public void setUnidadMedida(clsBeUnidad_medida value) {
+        UnidadMedida=value;
+    }
+
+    public clsBeProducto_Presentacion getPresentacion() {
+        return Presentacion;
+    }
+    public void setUbicacion(clsBeProducto_Presentacion value) {
+        Presentacion=value;
     }
 
     public Boolean getIsNew(){
