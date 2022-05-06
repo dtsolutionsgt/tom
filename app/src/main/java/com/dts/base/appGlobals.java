9b789766a2161bc2cb0251e1d7586f466e5dca5e
Carolina Fuentes <carolinakfk@hotmail.com>
2022-05-05 18:00:00 -0600

    //#CKFK20220505 otras validacones

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -196,7 +196,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="4.6.0.28";
+    public final String version="4.6.0.29";
 
     @Override
     public void onCreate() {
