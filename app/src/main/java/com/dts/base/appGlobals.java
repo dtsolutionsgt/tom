14fff65cc26b01701b209de012451b2989eeb50f
ejcalderongt <ejcalderon02@yahoo.com>
2022-06-18 10:59:33 -0600

    Task 144: Verificación, validar check Pendientes.

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -203,7 +203,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="4.6.1.0";
+    public final String version="4.6.1.1";
     public boolean VerificacionSinLoteFechaVen = false;
 
     //Voz Picking
