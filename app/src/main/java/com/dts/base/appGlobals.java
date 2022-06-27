8db439209d9f5a85efda5954ec60abfead44f643
Carolina Fuentes <carolinakfk@hotmail.com>
2022-06-27 01:04:48 -0600

    //#CKFK20220625 Correcciones en la recepcion para la lectura con código de barra

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -203,7 +203,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="4.6.1.9";
+    public final String version="4.6.2.0";
     public boolean VerificacionSinLoteFechaVen = false;
 
     //Voz Picking
