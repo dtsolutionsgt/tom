8a2ebf3260748f288c95e9b2e9f535cca16684d2
Carolina Fuentes <carolinakfk@hotmail.com>
2022-09-11 23:38:31 -0600

    #CKFK20220911 Cambio de versión

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -203,7 +203,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="4.6.3.11";
+    public final String version="4.6.3.12";
     public boolean VerificacionSinLoteFechaVen = false;
 
     //Voz Picking
