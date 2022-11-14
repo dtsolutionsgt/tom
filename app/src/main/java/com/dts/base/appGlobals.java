f2d47bf49ec086ce4d0f4779e31eb22c53dd5332
Carolina Fuentes <carolinakfk@hotmail.com>
2022-11-14 08:41:47 -0600

    #CKFK20221114 Quité los postdelayed

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -206,7 +206,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="4.6.3.23";
+    public final String version="4.6.3.24";
     public boolean VerificacionSinLoteFechaVen = false;
 
     //Voz Picking
