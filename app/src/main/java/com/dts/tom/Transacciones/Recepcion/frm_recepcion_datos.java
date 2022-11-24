0b4b0516f973f4f4a71826e3c22c8d1a4e2d9cd1
Efren Gustavo tuyuc <lemonde066@hotmail.com>
2022-11-24 11:45:21 -0600

    #GT03112022: comentarios prueba

--- app/src/main/java/com/dts/tom/Transacciones/Recepcion/frm_recepcion_datos.java
+++ app/src/main/java/com/dts/tom/Transacciones/Recepcion/frm_recepcion_datos.java
@@ -5217,7 +5217,7 @@ public class frm_recepcion_datos extends PBase {
 
                 //#GT19102022_0800: Mostrar el aviso con el delay
                 //#volvi a coloca el delay para impresión porque no se muestra nada.
-                //otro mensaje de prueba
+                //prueba de merge
                 progress.setMessage("Imprimiendo Código");
                 progress.show();
 
