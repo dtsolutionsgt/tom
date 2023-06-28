169fd6be153e3a0deb8a35ac71b9e1d79a811a3a
Carolina Fuentes <carolinakfk@hotmail.com>
2023-06-28 10:42:59 -0600

    #CKFK20230609 Cambio en el Picking

--- app/src/main/java/com/dts/tom/Transacciones/Picking/frm_picking_datos.java
+++ app/src/main/java/com/dts/tom/Transacciones/Picking/frm_picking_datos.java
@@ -2202,16 +2202,16 @@ public class frm_picking_datos extends PBase {
                         break;
                     case 7:
 
-                        /*callMethod("Actualizar_Picking",
-                                         "oBeTrans_picking_ubic",gBePickingUbic,
-                                         "BeStockRes",BeStockRes,
-                                         "IdBodega",gl.IdBodega,
-                                         "pCantidad",Double.parseDouble(txtCantidadPick.getText().toString().replace(",","")));
-                        */
-                        callMethod("Actualizar_Picking_From_HH",
+                        callMethod("Actualizar_Picking",
+                                   "oBeTrans_picking_ubic",gBePickingUbic,
+                                   "BeStockRes",BeStockRes,
+                                   "IdBodega",gl.IdBodega,
+                                   "pCantidad",Double.parseDouble(txtCantidadPick.getText().toString().replace(",","")));
+
+                        /*callMethod("Actualizar_Picking_From_HH",
                                 "oBeTrans_picking_ubic",gBePickingUbic,
                                 "IdBodega",gl.IdBodega,
-                                "pCantidad",Double.parseDouble(txtCantidadPick.getText().toString().replace(",","")));
+                                "pCantidad",Double.parseDouble(txtCantidadPick.getText().toString().replace(",","")));*/
 
                         break;
                     case 8:
