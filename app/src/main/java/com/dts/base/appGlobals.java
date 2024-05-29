233d5eb3216f6a0f1c080cdeea27d6b56f9c90a1
Carolina Fuentes <carolinakfk@hotmail.com>
2024-05-29 11:54:39 -0600

    #CKFK20240529 Funcionalidad de buscar los verificados y contados por licencia

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -219,7 +219,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="5.2.2";
+    public final String version="5.2.3";
     public boolean VerificacionSinLoteFechaVen = false;
 
     //Voz Picking
