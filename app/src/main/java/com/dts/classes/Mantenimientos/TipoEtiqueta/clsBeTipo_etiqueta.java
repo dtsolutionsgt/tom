3bf2a58876c71da3d2885152b974b82ee9d99bca
Anderly Teleguario <anderlyteleguario@outlook.com>
2025-03-20 10:42:29 -0600

    '#AT20250320 Agregue el campo Es_Inkjet en TipoEtiqueta

--- app/src/main/java/com/dts/classes/Mantenimientos/TipoEtiqueta/clsBeTipo_etiqueta.java
+++ app/src/main/java/com/dts/classes/Mantenimientos/TipoEtiqueta/clsBeTipo_etiqueta.java
@@ -21,6 +21,7 @@ public class clsBeTipo_etiqueta {
     @Element(required=false) public String codigo_zpl="";
 
     @Element(required=false) public int Idclasificacion_etiqueta = 0;//#CKFK20231008 Campo faltante
+    @Element(required=false) public boolean Es_Inkjet=false;
 
     public clsBeTipo_etiqueta() {
     }
@@ -28,7 +29,7 @@ public class clsBeTipo_etiqueta {
     public clsBeTipo_etiqueta(int IdTipoEtiqueta,String Nombre,double Alto,double Ancho,
                               double MargenIzq,double MagenDer,double MargenSup,double MargenInf,
                               String User_agr,String Fec_agr,String User_mod,String Fec_mod,
-                              boolean Activo,int dpi,String codigo_zpl, int Idclasificacion_etiqueta) {
+                              boolean Activo,int dpi,String codigo_zpl, int Idclasificacion_etiqueta, boolean Es_Inkjet) {
 
         this.IdTipoEtiqueta=IdTipoEtiqueta;
         this.Nombre=Nombre;
@@ -46,6 +47,7 @@ public class clsBeTipo_etiqueta {
         this.dpi=dpi;
         this.codigo_zpl=codigo_zpl;
         this.Idclasificacion_etiqueta = Idclasificacion_etiqueta;
+        this.Es_Inkjet = Es_Inkjet;
     }
 
 
@@ -150,6 +152,12 @@ public class clsBeTipo_etiqueta {
     }
     public void setIdclasificacion_etiqueta(int value) {
         Idclasificacion_etiqueta=value;
+    }
+    public boolean getEs_Inkjet() {
+        return Es_Inkjet;
+    }
+    public void setEs_Inkjet(boolean value) {
+        Es_Inkjet=value;
     }
 
 }
