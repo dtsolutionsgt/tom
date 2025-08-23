794beda1de3d86ec2673045cb2ee385ca034d28c
Carolina Fuentes <carolinakfk@hotmail.com>
2025-08-22 18:02:16 -0600

    #CKFK20250822 publicación con los campos nuevos

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -227,7 +227,7 @@ public class appGlobals extends Application {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public final String version="7.5.2";
+    public final String version="7.7.4";
     public boolean VerificacionSinLoteFechaVen = false;
     public String gBodega_Destino = "";
 
