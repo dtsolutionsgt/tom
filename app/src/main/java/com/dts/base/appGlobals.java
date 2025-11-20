7d3ba4ab3c6c202c8f8e08d02aef24cecaee5903
Efren Gustavo tuyuc <lemonde066@hotmail.com>
2025-11-19 18:04:31 -0600

    #GT19112025: variables para packing lote que no existen.

--- app/src/main/java/com/dts/base/appGlobals.java
+++ app/src/main/java/com/dts/base/appGlobals.java
@@ -274,6 +274,9 @@ public class appGlobals extends Application {
     public String LicenciaCUR = "";
     public boolean CambioUbicRecepcion = false;
     public boolean Control_Talla_Color = false;
+    public boolean Agrupar_Sin_Lic_Veri_No_Cons = false;
+    public boolean Advertir_Mpq_Umbas = false;
+
 
     @Override
     public void onCreate() {
